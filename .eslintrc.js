c0f9026a6235c84042353997a94ee6bcfc4e9dcc
d02ev <vikramadityapratapsingh98@gmail.com>
2024-07-22 22:42:47 +0530

    initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+	'env': {
+		'commonjs': true,
+		'es2021': true,
+		'node': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'overrides': [
+		{
+			'env': {
+				'node': true
+			},
+			'files': [
+				'.eslintrc.{js,cjs}'
+			],
+			'parserOptions': {
+				'sourceType': 'script'
+			}
+		}
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest'
+	},
+	'plugins': [
+		'@typescript-eslint'
+	],
+	'rules': {
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'ident': 'off',
+		'no-mixed-spaces-and-tabs': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-unsafe-member-access': 'off',
+		'@typescript-eslint/no-unsafe-assignment': 'off',
+		'@typescript-eslint/no-unsafe-argument': 'off',
+		'@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+	}
+};
